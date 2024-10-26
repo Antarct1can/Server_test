4ff17c82863072faad63c3e1583b0b97c167654d
Antarct1can <hussainb72@gmail.com>
2024-10-26 11:21:56 +0400

    123

--- pithermalcam/Test_server.py
+++ pithermalcam/Test_server.py
@@ -41,12 +41,6 @@ app = Flask(__name__)
 def index():
     return render_template("index - Copy.html")  # Assuming your HTML file is named index.html
 
-# @app.route("/video_feed")
-# def video_feed():
-# 	# return the response generated along with the specific media
-# 	# type (mime type)
-# 	return Response(generate(), mimetype="multipart/x-mixed-replace; boundary=frame")
-
 @app.route('/changeCameraBtn')
 def switch_camera():
     global thermal_camera, imx708_camera, usb_camera, current_camera
